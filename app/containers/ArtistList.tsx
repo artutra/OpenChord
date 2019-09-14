5820b46ccc91c603b0d951bc328d2951007950fe
Artur Miranda <ldm.artur@gmail.com>
2019-09-14 17:07:05 -0300

    Get artists from db on artist list screen

--- app/containers/ArtistList.tsx
+++ app/containers/ArtistList.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text } from "react-native";
 import { NavigationScreenProp } from "react-navigation"
 import { FlatList } from "react-native-gesture-handler";
+import realm, { Artist } from '../db'
 
 interface Props {
   navigation: NavigationScreenProp<any, any>
 }
 const ArtistList = (props: Props) => {
-  const [artists, setArtists] = useState([{ name: 'The Beatles', id: '1' }, { name: 'Coldplay', id: '2' }]);
+  const [artists, setArtists] = useState(realm.objects<Artist>('Artist'));
   return (
     <FlatList
       data={artists}
-      renderItem={({item}) =>{
-        return <Text onPress={() => props.navigation.navigate('ArtistView')} key={item.id}>{item.name}</Text>
+      renderItem={({ item}) =>{
+        return <Text onPress={() => props.navigation.navigate('ArtistView')} key={item.id!}>{item.name}</Text>
       }}
     />
   );
