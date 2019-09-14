72cb6acabf2ecbb05877091795bbace55b8b9ca1
Artur Miranda <ldm.artur@gmail.com>
2019-09-14 17:55:10 -0300

    Add listitem component to artist list screen and artist view screen, and add header titles

--- app/containers/ArtistList.tsx
+++ app/containers/ArtistList.tsx
@@ -3,19 +3,28 @@ import { Text } from "react-native";
 import { NavigationScreenProp } from "react-navigation"
 import { FlatList } from "react-native-gesture-handler";
 import realm, { Artist } from '../db'
+import ListItem from "../components/ListItem";
 
 interface Props {
   navigation: NavigationScreenProp<any, any>
 }
 const ArtistList = (props: Props) => {
   const [artists, setArtists] = useState(realm.objects<Artist>('Artist'));
+  function onSelectArtist(id: string, name: string) {
+    props.navigation.navigate('ArtistView', { id, title: name })
+  }
   return (
     <FlatList
       data={artists}
-      renderItem={({ item}) =>{
-        return <Text onPress={() => props.navigation.navigate('ArtistView', { id: item.id })} key={item.id!}>{item.name}</Text>
+      renderItem={({ item }) => {
+        return <ListItem key={item.id!} title={item.name} onPress={() => onSelectArtist(item.id!, item.name)} />
       }}
     />
   );
 }
+
+ArtistList.navigationOptions = (props: Props) => ({
+  title: 'Artists'
+});
+
 export default ArtistList
--- app/containers/ArtistView.tsx
+++ app/containers/ArtistView.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Text, FlatList } from "react-native";
 import { NavigationScreenProp } from "react-navigation"
 import realm, { Song, Artist } from "../db";
+import ListItem from "../components/ListItem";
 
 interface Props {
   navigation: NavigationScreenProp<any, any>
@@ -11,16 +12,21 @@ const ArtistView = (props: Props) => {
   let artist = realm.objectForPrimaryKey<Artist>('Artist', id)!
   const [name] = useState(artist.name)
   const [musics] = useState(realm.objects<Song>('Song').filtered('artist.id = $0', id));
+
+  function onSelectSong(id: string, title: string) {
+    props.navigation.navigate('SongView', { id, title })
+  }
   return (
     <FlatList
-      ListHeaderComponent={() => {
-        return <Text>{name}</Text>
-      }}
       data={musics}
-      renderItem={({item}) =>{
-        return <Text onPress={() => props.navigation.navigate('SongView', { id: item.id })} key={item.id!}>{item.title}</Text>
+      renderItem={({ item }) => {
+        return <ListItem key={item.id!} title={item.title} onPress={() => onSelectSong(item.id!, item.title)} />
       }}
     />
   );
 }
+ArtistView.navigationOptions = (props: Props) => ({
+  title: props.navigation.getParam('title')
+});
+
 export default ArtistView
