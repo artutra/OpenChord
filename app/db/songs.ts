c1ca46a899cae9ac4549dcd5a91c0c1fb830a87d
Artur Miranda <ldm.artur@gmail.com>
2019-09-14 17:51:38 -0300

    Add coldplay song mock

--- app/db/songs.ts
+++ app/db/songs.ts
@@ -12,6 +12,18 @@ const songs = [
   [F#m]Yesterday
   All my tr[B7]oubles seemed so f[Em]ar away[Em/D]
   [C] Now it l[D7]ooks as though they're h[G]ere to stay[G/F#]
-  Oh, [Em]I bel[A]ieve in y[C]est[G]erday`
+  Oh, [Em]I bel[A]ieve in y[C]est[G]erday`,
+  `{title: Yellow}
+  {artist: Coldplay}
+  Intro:
+  [B]  [B11]  [B]
+  [B]  [B11]  [B]
+  [B]  [F#6(11)]  [E7M]
+  
+  [Primeira Parte]
+  
+  [B]Look at the stars
+  Look how the shine f[F#6(11)]or you
+  And everything you d[E7M]o, yeah they were all yellow`
 ]
 export default songs
