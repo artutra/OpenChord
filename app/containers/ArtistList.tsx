c146aebff182564399ae46679968997beb95fd88
Artur Miranda <ldm.artur@gmail.com>
2019-09-23 23:57:42 -0300

    Populate db when open the first time

--- app/containers/ArtistList.tsx
+++ app/containers/ArtistList.tsx
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { NavigationScreenProp } from "react-navigation"
 import { FlatList } from "react-native-gesture-handler";
-import realm, { Artist } from '../db'
+import { Artist, Song } from '../db'
 import ListItem from "../components/ListItem";
 
 interface Props {
   navigation: NavigationScreenProp<any, any>
 }
 const ArtistList = (props: Props) => {
-  const [artists, setArtists] = useState(realm.objects<Artist>('Artist'));
+  const [artists, setArtists] = useState(Artist.getAll());
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (Song.shouldUpdateDb()) {
+      Song.populateDb()
+
+      setIsLoading(false)
+      setArtists(Artist.getAll())
+    }
+  }, [isLoading])
+
   function onSelectArtist(id: string, name: string) {
     props.navigation.navigate('ArtistView', { id, title: name })
   }
+
+  useEffect(() => {
+    const didBlurSubscription = props.navigation.addListener(
+      'didFocus',
+      payload => {
+        setArtists(Artist.getAll())
+      }
+    )
+    return () => didBlurSubscription.remove()
+  }, [artists])
+
   return (
-    <FlatList
-      data={artists}
-      renderItem={({ item }) => {
-        return <ListItem key={item.id!} title={item.name} onPress={() => onSelectArtist(item.id!, item.name)} />
-      }}
-    />
+    <View>
+      {isLoading && <Text>Is loading...</Text>}
+      <FlatList
+        data={artists}
+        renderItem={({ item }) => {
+          return <ListItem key={item.id!} title={item.name} onPress={() => onSelectArtist(item.id!, item.name)} />
+        }}
+      />
+    </View>
   );
 }
 
--- app/db/index.ts
+++ app/db/index.ts
@@ -1,5 +1,7 @@
 import Realm from 'realm'
 import uuid from 'uuid'
+import allSongs from '../assets/chordpro/songs.json'
+import ChordSheetJS from 'chordsheetjs';
 
 export class Song {
   id?: string | null
@@ -24,11 +26,33 @@ export class Song {
       artist: { type: 'Artist' }
     }
   }
+  static shouldUpdateDb() {
+    return this.getAll().length <= 0
+  }
+  static populateDb() {
+    if (this.shouldUpdateDb()) {
+      for (var i = 0; i < allSongs.length; i++) {
+        let s: string = allSongs[i]
+        const parser = new ChordSheetJS.ChordProParser();
+        const formatter = new ChordSheetJS.ChordProFormatter();
+        const parsedSong = parser.parse(s);
+
+        let artist = new Artist(parsedSong.getMetaData('artist')!)
+        let song = new Song(parsedSong.getMetaData('title')!, formatter.format(parsedSong), artist)
+        realm.write(() => {
+          Song.create(song)
+        })
+      }
+    }
+  }
+
+  static getAll() { return realm.objects<Song>('Song') }
+
   static create(song: Song) {
     let artistName = song.artist.name
     let existingArtist = realm.objects('Artist').filtered('name = $0', artistName).find(() => true)
     let createdArtist
-    if(existingArtist == null) {
+    if (existingArtist == null) {
       createdArtist = realm.create('Artist', {
         id: uuid(),
         name: artistName
@@ -45,8 +69,8 @@ export class Song {
 export class Artist {
   id?: string | null
   name: string
-  
-  constructor(name: string, id?:string) {
+
+  constructor(name: string, id?: string) {
     this.name = name
     this.id = id
   }
@@ -58,6 +82,7 @@ export class Artist {
       name: { type: 'string', optional: false }
     }
   }
+  static getAll() { return realm.objects<Artist>('Artist') }
 }
 var realm = new Realm({
   schema: [
@@ -65,6 +90,6 @@ var realm = new Realm({
     Artist.schema,
   ],
   schemaVersion: 2,
-  migration: () => {}
+  migration: () => { }
 })
 export default realm;
