a6086ada626153273f4c3807c915eb564b3c6ae8
Artur Miranda <ldm.artur@gmail.com>
2019-09-24 00:45:07 -0300

    Add updated_at field to models, improve create song method and use updated at to modify existing songs

--- app/db/index.ts
+++ app/db/index.ts
@@ -8,12 +8,14 @@ export class Song {
   title: string
   content: string
   artist: Artist
+  updated_at: Date
 
-  constructor(title: string, content: string, artist: Artist, id?: string, ) {
+  constructor(title: string, content: string, artist: Artist, updated_at: Date, id?: string, ) {
     this.id = id
     this.title = title
     this.content = content
     this.artist = artist
+    this.updated_at = updated_at
   }
 
   static schema: Realm.ObjectSchema = {
@@ -23,22 +25,29 @@ export class Song {
       id: 'string',
       title: 'string',
       content: 'string',
-      artist: { type: 'Artist' }
+      artist: { type: 'Artist' },
+      updated_at: 'date'
     }
   }
   static shouldUpdateDb() {
-    return this.getAll().length <= 0
+    let s = this.getAll().find(() => true)
+    let newSongsDate = new Date(allSongs.updated_at)
+    if (s == null)
+      return true
+    else
+      return newSongsDate > s.updated_at
   }
   static populateDb() {
     if (this.shouldUpdateDb()) {
-      for (var i = 0; i < allSongs.length; i++) {
-        let s: string = allSongs[i]
+      for (var i = 0; i < allSongs.data.length; i++) {
+        let s: string = allSongs.data[i]
+        let updated_at = new Date(allSongs.updated_at)
         const parser = new ChordSheetJS.ChordProParser();
         const formatter = new ChordSheetJS.ChordProFormatter();
         const parsedSong = parser.parse(s);
 
-        let artist = new Artist(parsedSong.getMetaData('artist')!)
-        let song = new Song(parsedSong.getMetaData('title')!, formatter.format(parsedSong), artist)
+        let artist = new Artist(parsedSong.getMetaData('artist')!, updated_at)
+        let song = new Song(parsedSong.getMetaData('title')!, formatter.format(parsedSong), artist, updated_at)
         realm.write(() => {
           Song.create(song)
         })
@@ -50,36 +59,51 @@ export class Song {
 
   static create(song: Song) {
     let artistName = song.artist.name
-    let existingArtist = realm.objects('Artist').filtered('name = $0', artistName).find(() => true)
-    let createdArtist
-    if (existingArtist == null) {
-      createdArtist = realm.create('Artist', {
+    let artist: Artist | undefined
+    artist = realm.objects<Artist>('Artist').filtered('name = $0', artistName).find(() => true)
+    if (artist == null) {
+      artist = realm.create<Artist>('Artist', {
         id: uuid(),
-        name: artistName
+        name: artistName,
+        updated_at: song.artist.updated_at
       })
     }
-    realm.create('Song', {
-      id: uuid(),
-      title: song.title,
-      content: song.content,
-      artist: existingArtist == null ? createdArtist : existingArtist
-    })
+    let songDb: Song | undefined
+    songDb = realm
+      .objects<Song>('Song')
+      .filtered('title = $0 && artist.name = $1', song.title, artistName)
+      .find(() => true)
+    if (songDb == null) {
+      songDb = realm.create('Song', {
+        id: uuid(),
+        title: song.title,
+        content: song.content,
+        artist,
+        updated_at: song.updated_at
+      })
+    } else {
+      songDb.content = song.content
+      songDb.updated_at = song.updated_at
+    }
   }
 }
 export class Artist {
   id?: string | null
   name: string
+  updated_at: Date
 
-  constructor(name: string, id?: string) {
+  constructor(name: string, updated_at: Date, id?: string) {
     this.name = name
     this.id = id
+    this.updated_at = updated_at
   }
   static schema: Realm.ObjectSchema = {
     name: 'Artist',
     primaryKey: 'id',
     properties: {
       id: 'string',
-      name: { type: 'string', optional: false }
+      name: { type: 'string', optional: false },
+      updated_at: 'date'
     }
   }
   static getAll() { return realm.objects<Artist>('Artist') }
@@ -89,7 +113,7 @@ var realm = new Realm({
     Song.schema,
     Artist.schema,
   ],
-  schemaVersion: 2,
+  schemaVersion: 3,
   migration: () => { }
 })
 export default realm;
