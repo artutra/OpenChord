36b7d98cd3811eda56ba8b2e87e5feb667393a4d
Artur Miranda <ldm.artur@gmail.com>
2019-09-26 22:30:54 -0300

    Add SongRender to SongView and include transpose song buttons in a side menu

--- app/containers/SongView.tsx
+++ app/containers/SongView.tsx
@@ -1,89 +1,75 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
-import realm, { Song } from '../db'
-import { NavigationScreenProp } from "react-navigation";
-import ChordSheetJS from 'chordsheetjs'
-import WebView from 'react-native-webview'
+import React, { useState, useEffect, FunctionComponent } from "react";
+import { Text, View } from "react-native";
+import { Song } from '../db'
+import { NavigationScreenComponent } from "react-navigation";
+import SideMenu from 'react-native-side-menu'
+import SongRender from "../components/SongRender";
+import TouchableIcon from "../components/TouchableIcon";
+import { NavigationStackOptions, NavigationStackProp, } from "react-navigation-stack/lib/typescript/types";
 
-interface Props {
-  navigation: NavigationScreenProp<any, { id: string, title: string }>
+type Params = { id: string, title: string, openSideMenu: () => void }
+
+type Props = {
+  navigation: NavigationStackProp<{}, Params>
 }
-const SongView = (props: Props) => {
-  let id = props.navigation.getParam('id')
-  let song = realm.objectForPrimaryKey<Song>('Song', id)!
-  const parser = new ChordSheetJS.ChordProParser();
-  const formatter = new ChordSheetJS.HtmlDivFormatter();
-  const parsedSong = parser.parse(song.content);
-  const [content] = useState(formatter.format(parsedSong))
-  const headers = `<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">`
-  function renderHtml(headers: string, body: string, styles: string) {
-    return `<html>
-      <head>${headers}</head>
-      <body>${body}</body>
-      <style>${styles}</style>
-    </html>`
-  }
+
+const SongView: FunctionComponent<Props> & NavigationScreenComponent<
+  NavigationStackOptions,
+  NavigationStackProp
+> = (props) => {
+  const [content, setContent] = useState<string>("")
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState<boolean>(false)
+  const [tone, setTone] = useState<number>(0)
+
+  function transposeUp() { setTone(tone + 1 >= 12 ? 0 : tone + 1) }
+  function transposeDown() { setTone(tone - 1 <= -12 ? 0 : tone - 1) }
+  function openSideMenu() { setIsSideMenuOpen(!isSideMenuOpen) }
 
   function onClickChord(chord: string) {
     // TODO: Show guitar chord diagram
   }
 
+  useEffect(() => {
+    let id = props.navigation.getParam('id')
+    let song = Song.getById(id)!
+    setContent(song.content)
+  }, [content])
+
+  useEffect(() => {
+    props.navigation.setParams({ 'openSideMenu': openSideMenu })
+  }, [isSideMenuOpen])
+
   return (
-    <WebView
-      startInLoadingState={true}
-      overScrollMode={'never'}
-      scrollEnabled={false}
-      source={{ html: renderHtml(headers, content, styles) }}
-      injectedJavaScript={onClickChordPostMessage}
-      onMessage={(event) => { onClickChord(event.nativeEvent.data) }}
-    />
+    <SideMenu
+      menu={
+        <View style={{ backgroundColor: '#eee', flex: 1 }}>
+          <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#ccc' }}>
+            <TouchableIcon onPress={transposeUp} name="plus" />
+            <Text>{tone}</Text>
+            <TouchableIcon onPress={transposeDown} name="minus" />
+          </View>
+        </View>
+      }
+      onChange={(isOpen) => { setIsSideMenuOpen(isOpen) }}
+      openMenuOffset={50}
+      isOpen={isSideMenuOpen}
+      menuPosition="right"
+      disableGestures={true}
+    >
+      <SongRender
+        onPressChord={onClickChord}
+        chordProContent={content}
+        tone={tone}
+      />
+    </SideMenu>
   );
 }
-const onClickChordPostMessage = `
-(
-  function() {
-    function onClickChord (chord) {
-      return function () {
-        window.ReactNativeWebView.postMessage(chord)
-      }
-    }
-  var anchors = document.getElementsByClassName('chord');
-  for(var i = 0; i < anchors.length; i++) {
-      var anchor = anchors[i];
-      var chord = anchor.innerText || anchor.textContent;
-      anchor.onclick = onClickChord(chord)
-  }
-})();
 
-true;
-`
-const styles = `
-.chord {
-  color: red;
-  font-size: 16px;
+SongView.navigationOptions = ({ navigation }) => {
+  return {
+    title: navigation.getParam('title'),
+    headerRight: <TouchableIcon onPress={navigation.getParam('openSideMenu')} name="settings" />,
+  }
 }
-.chord:active {
-  color: blue;
-}
-.lyrics {
-  font-size: 16px;
-}
-.column {
-  display: inline-block;
-}
-.row {
-  position: relative;
-  margin-bottom: 0px;
-  font-family: monospace;
-  white-space: pre-wrap;
-  margin-right: 10px;
-  font-size: 23px;
-}
-p {
-	margin: 0;
-}
-`
-SongView.navigationOptions = (props: Props) => ({
-  title: props.navigation.getParam('title')
-});
+
 export default SongView
