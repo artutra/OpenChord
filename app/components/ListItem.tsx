d3f42149c401ab1f942d154fc960f533d473cd6f
Artur Miranda <ldm.artur@gmail.com>
2019-10-04 14:40:47 -0300

    Add subtitle prop to ListItem component

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, FunctionComponent } from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
-interface Props {
+interface ListItemProps {
   title: string
   subtitle?: string
   onPress: () => void
 }
-const ListItem = (props: Props) => {
+const ListItem: FunctionComponent<ListItemProps> = (props) => {
   return (
     <TouchableOpacity onPress={props.onPress} style={styles.item}>
-      <Text style={styles.itemTitle}>{props.title}</Text>
+      <Text style={styles.title}>{props.title}</Text>
+      {props.subtitle && <Text style={styles.subtitle}>{props.subtitle}</Text>}
     </TouchableOpacity>
   );
 }
@@ -25,7 +26,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'flex-start'
   },
-  itemTitle: {
+  title: {
     fontSize: 18
+  },
+  subtitle: {
+    fontSize: 14
   }
 });
