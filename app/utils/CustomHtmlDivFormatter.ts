f5d0fa76d500ccc378453ebfcdc15da1e11b90a3
Artur Miranda <ldm.artur@gmail.com>
2019-10-12 10:10:51 -0300

    Add spaces between chords and remove unecessary NEW_LINE on the end

--- app/utils/CustomHtmlDivFormatter.ts
+++ app/utils/CustomHtmlDivFormatter.ts
@@ -1,14 +1,18 @@
 import ChordSheetJS, { Song } from "chordsheetjs"
+const NEW_LINE = '\n'
 
 export default class CustomHtmlDivFormatter {
   format(song: Song) {
     let html = ''
-    song.lines.forEach(l => {
+    song.lines.forEach((l, index) => {
       html += `<p class="line">`
       l.items.forEach(item => {
         if (item instanceof ChordSheetJS.ChordLyricsPair) {
           if (item.chords) {
-            html += `<span class="chord">${item.chords}</span>${item.lyrics}`
+            let { lyrics } = item
+            if (lyrics.length <= item.chords.length)
+              lyrics = lyrics + " ".repeat(item.chords.length - lyrics.length + 1)
+            html += `<span class="chord">${item.chords}</span>${lyrics}`
           } else {
             html += `${item.lyrics}`
           }
@@ -18,7 +22,10 @@ export default class CustomHtmlDivFormatter {
           }
         }
       })
-      html += `</p>\n`
+      html += `</p>`
+      if (index < song.lines.length - 1) {
+        html += NEW_LINE
+      }
     })
     html = html.replace(/\w+(<span class="chord">(.*?)<\/span>\w+)+/g, (v) => {
       return `<span class="word">${v}</span>`
