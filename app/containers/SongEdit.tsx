284308d2a2ff06c577dd702db1950b36b39de785
Artur Miranda <ldm.artur@gmail.com>
2019-10-14 09:53:15 -0300

    Add SongEdit container and configure navigation

---
+++ app/containers/SongEdit.tsx
@@ -0,0 +1,190 @@
+import React, { useState, useEffect, FunctionComponent } from "react";
+import { Text, View, StyleSheet, TextInput, KeyboardAvoidingView, Button } from "react-native";
+import { Song, Artist } from '../db'
+import { NavigationScreenComponent } from "react-navigation";
+import TouchableIcon from "../components/TouchableIcon";
+import { NavigationStackOptions, NavigationStackProp, } from "react-navigation-stack/lib/typescript/types";
+import ChordSheetJS from 'chordsheetjs'
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import realm from '../db'
+
+type Params = { id: string | null | undefined, title: string, saveSong: () => void }
+
+type Props = {
+  navigation: NavigationStackProp<{}, Params>
+}
+
+const SongEdit: FunctionComponent<Props> & NavigationScreenComponent<
+  NavigationStackOptions,
+  NavigationStackProp
+> = (props) => {
+  const [title, setTitle] = useState("")
+  const [artist, setArtist] = useState("")
+  const [content, setContent] = useState("")
+  const [error, setError] = useState<string | null>(null)
+  const [mode, setMode] = useState<'CHORD_PRO' | 'CHORD_SHEET'>('CHORD_PRO')
+
+  useEffect(() => {
+    let id = props.navigation.getParam('id')
+    if (id != null) {
+      let song = Song.getById(id)!
+      setTitle(song.title)
+      setArtist(song.artist.name)
+      setContent(song.content)
+    }
+  }, [])
+
+  function saveSong() {
+    if (title.trim() == '') return setError('Invalid Title')
+    if (artist.trim() == '') return setError('Invalid Artist')
+    if (content.trim() == '') return setError('Invalid content')
+    let header =
+      `{title: ${title.trim()}}\n` +
+      `{artist: ${artist.trim()}}\n`
+    let chordPro: string
+    if (mode == 'CHORD_PRO') {
+      chordPro = header + content
+    } else {
+      const formatter = new ChordSheetJS.ChordProFormatter();
+      let chordSheetSong = new ChordSheetJS.ChordSheetParser({ preserveWhitespace: true }).parse(content)
+      chordPro = header + formatter.format(chordSheetSong)
+    }
+    let songId = props.navigation.getParam('id')
+    let artistDb = new Artist(artist.trim())
+    let song = new Song(title.trim(), chordPro, artistDb, new Date(), songId)
+    realm.write(() => {
+      let created = Song.create(song)
+      songId = created.id
+    })
+    // TODO: Remove old song if title was changed 
+    props.navigation.navigate('SongView', { id: songId, title: title.trim() })
+  }
+
+  useEffect(() => {
+    props.navigation.setParams({ 'saveSong': saveSong })
+  }, [artist, title, content])
+
+  function switchToChordPro() {
+    let song = new ChordSheetJS.ChordSheetParser({ preserveWhitespace: true }).parse(content)
+    let chordPro = new ChordSheetJS.ChordProFormatter().format(song)
+    setContent(chordPro)
+    setMode('CHORD_PRO')
+  }
+  function switchToChordSheet() {
+    let song = new ChordSheetJS.ChordProParser().parse(content)
+    let plainText = new ChordSheetJS.TextFormatter().format(song)
+    setContent(plainText)
+    setMode('CHORD_SHEET')
+  }
+
+  const contentPlaceholder = mode == 'CHORD_PRO' ?
+    "You can edit any song here\n" +
+    "U[C]sing the [Dm]chordPro format[G]\n\n\n" :
+    "You can edit any song here\n" +
+    " C              Dm          G\n" +
+    "Using the chord sheet format\n\n\n"
+
+  return (
+    <ScrollView
+      contentContainerStyle={styles.container}
+      keyboardDismissMode="none"
+    >
+      <KeyboardAvoidingView>
+        {error != null && <Text style={{ color: 'red' }}>{error}</Text>}
+        <TextInput
+          style={styles.input}
+          placeholder="Song Title"
+          autoFocus={false}
+          autoCorrect={false}
+          autoCapitalize='words'
+          onChangeText={setTitle}
+          value={title}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Artist Name"
+          autoFocus={false}
+          autoCorrect={false}
+          autoCapitalize='words'
+          onChangeText={setArtist}
+          value={artist}
+        />
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={mode == 'CHORD_PRO' ? styles.tabActive : styles.tabInactive}
+            onPress={switchToChordPro} disabled={mode == 'CHORD_PRO'}>
+            <Text>ChordPro</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={mode == 'CHORD_SHEET' ? styles.tabActive : styles.tabInactive}
+            onPress={switchToChordSheet} disabled={mode == 'CHORD_SHEET'}>
+            <Text>Chord over lyrics</Text>
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          textAlignVertical="top"
+          style={styles.content}
+          placeholder={contentPlaceholder}
+          numberOfLines={4}
+          placeholderTextColor="#aaa"
+          multiline
+          autoFocus={false}
+          autoCorrect={false}
+          autoCapitalize='sentences'
+          onChangeText={setContent}
+          value={content}
+        />
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
+}
+
+SongEdit.navigationOptions = ({ navigation }) => {
+  return {
+    title: 'Edit Song',//navigation.getParam('title'),
+    headerRight: <TouchableIcon onPress={navigation.getParam('saveSong')} name="content-save" />,
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  },
+  tabActive: {
+    borderTopRightRadius: 3,
+    borderTopLeftRadius: 3,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#eee'
+  },
+  tabInactive: {
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff'
+  },
+  input: {
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    marginBottom: 5
+  },
+  content: {
+    fontFamily: 'monospace',
+    flex: 1,
+    minHeight: 200,
+    padding: 10,
+    backgroundColor: '#eee',
+    borderBottomLeftRadius: 3,
+    borderBottomRightRadius: 3,
+  },
+  sideMenuContainer: {
+    backgroundColor: '#eee',
+    flex: 1
+  },
+  toolbarContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ccc'
+  }
+})
+export default SongEdit
