6589b2f57a013c2a7b3de84d140a4b38b0cc1a55
Artur Miranda <ldm.artur@gmail.com>
2019-10-15 16:41:10 -0300

    Fix remove tabs regex

--- app/components/SongTransformer.tsx
+++ app/components/SongTransformer.tsx
@@ -42,7 +42,7 @@ const SongTransformer: FunctionComponent<Props> = (props) => {
   let song: Song
   if (chordProSong != null) {
     if (!showTabs) {
-      chordProSong = chordProSong.replace(/{sot}(.*\r\n)*?{eot}/g, '')
+      chordProSong = chordProSong.replace(/{sot}(.*\r?\n)*?{eot}\r?\n?/g, '')
     }
     song = new ChordSheetJS.ChordProParser().parse(chordProSong);
   } else {
