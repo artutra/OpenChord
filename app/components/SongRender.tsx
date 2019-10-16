8b3d07359bfff1d37c513bbb0e39d435466ab00b
Artur Miranda <ldm.artur@gmail.com>
2019-10-15 22:39:59 -0300

    Add class style to all sizes available

--- app/components/SongRender.tsx
+++ app/components/SongRender.tsx
@@ -64,17 +64,37 @@ body {
   white-space: pre-wrap;
   margin-right: 10px;
 }
+.line-size-14 { font-size: 14px; }
+.line-size-15 { font-size: 15px; }
+.line-size-16 { font-size: 16px; }
+.line-size-17 { font-size: 17px; }
+.line-size-18 { font-size: 18px; }
+.line-size-19 { font-size: 19px; }
+.line-size-20 { font-size: 20px; }
+.line-size-21 { font-size: 21px; }
+.line-size-22 { font-size: 22px; }
+.line-size-23 { font-size: 23px; }
+.line-size-24 { font-size: 24px; }
 .chord {
   color: red;
   position: relative;
   display: inline-block;
-  line-height: 20px;
   padding-top: 20px;
-  padding-top: 15px;
   width: 0px;
   top: -17px;
   cursor: pointer;
 }
+.chord-size-14 { top: -14px; }
+.chord-size-15 { top: -15px; }
+.chord-size-16 { top: -16px; }
+.chord-size-17 { top: -17px; }
+.chord-size-18 { top: -18px; }
+.chord-size-19 { top: -19px; }
+.chord-size-20 { top: -20px; }
+.chord-size-21 { top: -21px; }
+.chord-size-22 { top: -22px; }
+.chord-size-23 { top: -23px; }
+.chord-size-24 { top: -24px; }
 .chord:active {
   color: blue;
 }
