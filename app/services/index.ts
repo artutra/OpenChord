4a045aa44080b2ec96da837d8fb60730c824c88c
Artur Miranda <ldm.artur@gmail.com>
2019-10-25 18:20:10 -0300

    Remove Cifraclub services and parsers

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,8 +1,7 @@
 import { BaseService } from "./BaseService"
-import Cifraclub from "./CifraclubService"
 import OpenChordService from "./OpenChordService"
 
-export const services: BaseService[] = [new Cifraclub(), new OpenChordService()]
+export const services: BaseService[] = [new OpenChordService()]
 export const getService = (serviceName: string) => {
   return services.find(s => s.name == serviceName)
 }
