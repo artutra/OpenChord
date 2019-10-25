84b142b2c84d012ef937cac120b886c16acb0d44
Artur Miranda <ldm.artur@gmail.com>
2019-10-25 18:15:49 -0300

    Add OpenChordAPI service

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,7 +1,8 @@
 import { BaseService } from "./BaseService"
 import Cifraclub from "./CifraclubService"
+import OpenChordService from "./OpenChordService"
 
-export const services: BaseService[] = [new Cifraclub()]
+export const services: BaseService[] = [new Cifraclub(), new OpenChordService()]
 export const getService = (serviceName: string) => {
   return services.find(s => s.name == serviceName)
 }
