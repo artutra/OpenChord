cb0982101f103c02363c20de675e5258c48b40e9
Artur Miranda <ldm.artur@gmail.com>
2019-11-19 00:15:09 -0300

    Add menu options to ArtistView and SongList songs

--- app/containers/SongList.tsx
+++ app/containers/SongList.tsx
@@ -5,6 +5,7 @@ import { Song } from '../db'
 import ListItem from "../components/ListItem";
 import TouchableIcon from "../components/TouchableIcon";
 import { NavigationStackOptions, NavigationStackProp } from "react-navigation-stack/lib/typescript/types";
+import { removeSong } from "../utils/removeSong";
 
 interface Props {
   navigation: NavigationScreenProp<any, { addSong: () => void }>
@@ -13,14 +14,25 @@ const SongList: FunctionComponent<Props> & NavigationScreenComponent<
   NavigationStackOptions,
   NavigationStackProp
 > = (props: Props) => {
-  const [songs] = useState(Song.getAll());
+  const [songs, setSongs] = useState(Song.getAll());
   function onSelectSong(id: string, title: string) {
     props.navigation.navigate('SongView', { id, title })
   }
   function addNewSong() {
     props.navigation.navigate('SongEdit')
   }
-
+  function onPressEditSong(id: string) {
+    props.navigation.navigate('SongEdit', { id })
+  }
+  function onPressGoToArtist(id: string) {
+    let artist = Song.getById(id)!.artist
+    props.navigation.navigate('ArtistView', { id: artist.id, title: artist.name })
+  }
+  function onPressDeleteSong(id: string) {
+    removeSong(id, () => {
+      setSongs(Song.getAll())
+    })
+  }
   useEffect(() => {
     props.navigation.setParams({ 'addSong': addNewSong })
   }, [songs])
@@ -29,7 +41,19 @@ const SongList: FunctionComponent<Props> & NavigationScreenComponent<
     <FlatList
       data={songs}
       renderItem={({ item }) => {
-        return <ListItem key={item.id!} title={item.title} onPress={() => onSelectSong(item.id!, item.title)} />
+        return (
+          <ListItem
+            key={item.id!}
+            title={item.title}
+            subtitle={item.artist.name}
+            onPress={() => onSelectSong(item.id!, item.title)}
+            options={[
+              { title: 'Go to Artist', onPress: () => onPressGoToArtist(item.id!) },
+              { title: 'Edit', onPress: () => onPressEditSong(item.id!) },
+              { title: 'Delete', onPress: () => onPressDeleteSong(item.id!) }
+            ]}
+          />
+        )
       }}
     />
   );
