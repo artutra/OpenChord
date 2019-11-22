4cc32dab2af11402d7d390fc95c1fa01442ce6cd
Artur Miranda <ldm.artur@gmail.com>
2019-11-21 21:56:16 -0300

    Add hasSong,addSong and removeSong static functions to Playlist model

--- app/db/Playlist.ts
+++ app/db/Playlist.ts
@@ -6,7 +6,7 @@ import uuid from "uuid"
 export class Playlist {
   id!: string
   name!: string
-  songs!: List<Song>
+  songs!: List<Song> | Song[]
   updated_at!: Date
 
   static schema: Realm.ObjectSchema = {
@@ -21,6 +21,20 @@ export class Playlist {
   }
   static getAll() { return realm.objects<Playlist>('Playlist').sorted('name') }
   static getById(id: string) { return realm.objectForPrimaryKey<Playlist>('Playlist', id) }
+  static hasSong(playlist: Playlist, song: Song) {
+    if (!playlist || !song) return false
+    return playlist.songs.some(s => s.id == song.id)
+  }
+  static addSong(playlist: Playlist, song: Song) {
+    realm.write(() => {
+      playlist.songs.push(song)
+    })
+  }
+  static removeSong(playlist: Playlist, song: Song) {
+    realm.write(() => {
+      playlist.songs = playlist.songs.filter(s => s.id != song.id)
+    })
+  }
   static create(name: string) {
     // TODO: check if exists
     let playlist: Playlist
