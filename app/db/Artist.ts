fccd300957d5ccdabe48a17afbb99bb28ed6a07e
Artur Miranda <ldm.artur@gmail.com>
2019-11-23 12:45:28 -0300

    Remove Artist constructor, add getByName and refactor create functions

--- app/db/Artist.ts
+++ app/db/Artist.ts
@@ -1,16 +1,12 @@
 import realm from "."
 import { Song } from "./Song"
+import uuid from 'uuid'
 
 export class Artist {
-  id?: string | null
-  name: string
-  updated_at: Date
+  id!: string
+  name!: string
+  updated_at!: Date
 
-  constructor(name: string, updated_at?: Date, id?: string) {
-    this.name = name
-    this.id = id
-    this.updated_at = updated_at ? updated_at : new Date()
-  }
   static schema: Realm.ObjectSchema = {
     name: 'Artist',
     primaryKey: 'id',
@@ -25,7 +21,9 @@ export class Artist {
     return Song.getByArtist(this.id!)
   }
   static getAll() { return realm.objects<Artist>('Artist').sorted('name') }
-
+  static getByName(name: string) {
+    return realm.objects<Artist>('Artist').filtered('name = $0', name).find(() => true)
+  }
   static getById(id: string) {
     return realm.objectForPrimaryKey<Artist>('Artist', id)
   }
@@ -34,5 +32,23 @@ export class Artist {
       realm.delete(Song.getByArtist(id))
       realm.delete(this.getById(id))
     })
+  }
+  static create(name: string) {
+    if (name == null || name == "") {
+      throw new Error(`Empty name not allowed`)
+    }
+    let sameNameArtist = Artist.getByName(name)
+    if (sameNameArtist) {
+      throw new Error(`Artist with name "${name}" already exists`)
+    }
+    let artist: Artist
+    realm.write(() => {
+      artist = realm.create<Artist>('Artist', {
+        id: uuid(),
+        name,
+        updated_at: new Date()
+      })
+    })
+    return artist!
   }
 }
