d952cbd8fb14f2830cfb67b6be125b8caa17d631
Artur Miranda <ldm.artur@gmail.com>
2019-11-23 12:47:26 -0300

    Upgrade deprecated db methods calls from screens

--- app/containers/SongPreview.tsx
+++ app/containers/SongPreview.tsx
@@ -7,7 +7,7 @@ import SongTransformer from "../components/SongTransformer";
 import { getService } from "../services";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import ChordSheetJS from 'chordsheetjs';
-import realm, { Artist, Song } from "../db";
+import { Artist, Song } from "../db";
 import { ROUTES } from "../AppNavigation";
 import { SongViewParams } from "./SongView";
 
@@ -39,14 +39,17 @@ const SongPreview: FunctionComponent<SongPreviewProps> & NavigationScreenCompone
     const formatter = new ChordSheetJS.ChordProFormatter();
     const parsedSong = parser.parse(chordSheet!);
 
-    let artist = new Artist(parsedSong.getMetaData('artist')!)
-    let song = new Song(parsedSong.getMetaData('title')!, formatter.format(parsedSong), artist)
-    let songId: string
-    realm.write(() => {
-      let created = Song.create(song)
-      songId = created.id!
-    })
-    let params: SongViewParams = { id: songId!, title: song.title }
+    let artistName = parsedSong.getMetaData('artist')!
+    let songTitle = parsedSong.getMetaData('title')!
+    let songContent = formatter.format(parsedSong)
+
+    let artist: Artist | undefined = Artist.getByName(artistName)
+    if (artist == null) {
+      artist = Artist.create(artistName)
+    }
+    let song = Song.create(artist, songTitle, songContent)
+
+    let params: SongViewParams = { id: song.id, title: song.title }
     props.navigation.replace(ROUTES.SongView, params)
     Alert.alert('Info', 'Song downloaded')
   }
