652caa63331e3c839da4f5d6da7ef129f8e3799a
Artur Miranda <ldm.artur@gmail.com>
2019-11-24 23:32:24 -0300

    Render tab divs for {sot} {eot} tags

--- app/utils/CustomHtmlDivFormatter.ts
+++ app/utils/CustomHtmlDivFormatter.ts
@@ -1,12 +1,78 @@
-import ChordSheetJS, { Song } from "chordsheetjs"
+import ChordSheetJS, { Song, Tag, Line } from "chordsheetjs"
 const NEW_LINE = '\n'
 
 export default class CustomHtmlDivFormatter {
+  private isStartOfTabs(line: Line) {
+    return line.items.some(i => i instanceof ChordSheetJS.Tag && i.name == 'sot')
+  }
+  private isEndOfTabs(line: Line) {
+    return line.items.some(i => i instanceof ChordSheetJS.Tag && i.name == 'eot')
+  }
+  private getLyrics(line: Line) {
+    let lyrics = ""
+    line.items.forEach(item => {
+      if (item instanceof ChordSheetJS.ChordLyricsPair) {
+        lyrics = lyrics + item.lyrics
+      }
+    })
+    return lyrics
+  }
+  private getTabStringArray(song: Song, startAtIndex: number) {
+    let tabArray: string[] = []
+    let line = song.lines[startAtIndex]
+    do {
+      let tabLine = this.getLyrics(line)
+      if (tabLine != '') {
+        tabArray.push(tabLine)
+      }
+      startAtIndex++
+      line = song.lines[startAtIndex]
+    } while (line != null && !this.isEndOfTabs(line))
+    return tabArray
+  }
+  private transposeArray(array: string[]) {
+    let biggestLine = 0
+    array.forEach(line => biggestLine = Math.max(line.length, biggestLine))
+
+    let transposedArray: string[] = [];
+    for (let i = 0; i < biggestLine; i++) {
+      transposedArray.push('')
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < biggestLine; j++) {
+        let character = array[i][j] ? array[i][j] : ' '
+        transposedArray[j] = transposedArray[j] + character
+      }
+    }
+    return transposedArray
+  }
+
   format(song: Song, fontSize = 14) {
     let CHORD_SIZE_CLASS = fontSize != 14 ? ` chord-size-${fontSize}` : ''
     let LYRICS_SIZE_CLASS = fontSize != 14 ? ` line-size-${fontSize}` : ''
     let html = ''
+    let waitEndOfTabs = false
     song.lines.forEach((l, index) => {
+      if (waitEndOfTabs) {
+        if (this.isEndOfTabs(l)) {
+          waitEndOfTabs = false
+        }
+        // Skip line
+        return
+      }
+      if (this.isStartOfTabs(l)) {
+        waitEndOfTabs = true
+        let tabStringArray = this.getTabStringArray(song, index)
+        let transposedArray = this.transposeArray(tabStringArray)
+        html += `<div class="tab${LYRICS_SIZE_CLASS}">`
+        transposedArray.forEach(tabLine => {
+          html += `<div class="tab-line">${tabLine}</div>` + NEW_LINE
+        })
+        html += `</div>` + NEW_LINE
+        // Skip line
+        return
+      }
       html += `<p class="line${LYRICS_SIZE_CLASS}">`
       l.items.forEach(item => {
         if (item instanceof ChordSheetJS.ChordLyricsPair) {
