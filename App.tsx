d48531aad4544e6aab59b88e9535c35d4f0742a1
Artur Miranda <ldm.artur@gmail.com>
2020-06-02 18:52:46 -0300

    Configure Language as a global context

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import AppNavigation from './app/AppNavigation'
 import { createAppContainer } from "react-navigation";
+import { LanguageProvider } from "./app/languages/LanguageContext";
 
 const AppContainer = createAppContainer(AppNavigation);
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppContainer />
+      <LanguageProvider>
+        <AppContainer />
+      </LanguageProvider>
     );
   }
 }
