dbafa290ca235701967644477b1351a9ea470c2d
Artur Miranda <ldm.artur@gmail.com>
2020-06-04 14:14:36 -0300

    Load language settings when start app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import AppNavigation from './app/AppNavigation'
 import { createAppContainer } from "react-navigation";
-import { LanguageProvider } from "./app/languages/LanguageContext";
+import LanguageContext, { LanguageProvider } from "./app/languages/LanguageContext";
+import { GlobalSettings } from "./app/db/GlobalSettings";
 
 const AppContainer = createAppContainer(AppNavigation);
+
+const LoadLanguage = () => {
+  const { changeLanguage } = useContext(LanguageContext)
+  useEffect(() => {
+    let { language } = GlobalSettings.get()
+    changeLanguage(language)
+  }, [])
+  return null
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <LanguageProvider>
+        <LoadLanguage />
         <AppContainer />
       </LanguageProvider>
     );
