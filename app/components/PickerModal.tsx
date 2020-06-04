8ed7692deb3dc166b91d7742a4bc1fff7c1b41cc
Artur Miranda <ldm.artur@gmail.com>
2020-06-04 18:13:46 -0300

    Add PickerModal component

---
+++ app/components/PickerModal.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, TouchableOpacity, StyleSheet, Modal } from "react-native";
+import ListItem from "./ListItem";
+
+interface Props<T> {
+  show: boolean
+  value: T
+  options: {
+    label: string
+    description?: string
+    value: T
+  }[]
+  onChange: (name: T) => void
+  onDismiss: () => void
+}
+
+function PickerModal<T>(props: Props<T>) {
+  const {
+    value,
+    show,
+    onDismiss,
+    options,
+    onChange
+  } = props
+
+  if (!show) return null
+
+  return (
+    <Modal transparent onDismiss={onDismiss}>
+      <View style={styles.backgroundOverlayer}>
+        <TouchableOpacity style={styles.outsideContainer} onPress={onDismiss} />
+        <View style={styles.container}>
+          {options.map(o => {
+            return (
+              <ListItem
+                onPress={() => onChange(o.value)}
+                title={o.label}
+                subtitle={o.description}
+                showIcon={o.value === value ? 'check' : null} />
+            )
+          })}
+        </View>
+      </View>
+    </Modal>
+  );
+}
+export default PickerModal
+
+const styles = StyleSheet.create({
+  backgroundOverlayer: {
+    backgroundColor: '#00000040',
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  outsideContainer: {
+    flex: 1
+  },
+  container: {
+    backgroundColor: 'white'
+  }
+});
