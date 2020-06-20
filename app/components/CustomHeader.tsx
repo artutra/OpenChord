ae16f470abef7a3dde00259999ad67e587e6f4bc
Artur Miranda <ldm.artur@gmail.com>
2020-06-20 19:44:10 -0300

    Improve custom header style for ios

--- app/components/CustomHeader.tsx
+++ app/components/CustomHeader.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, ReactNode } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Platform } from "react-native";
 
 interface Props {
   title: string
@@ -8,26 +8,38 @@ interface Props {
 const CustomHeader: FunctionComponent<Props> = ({ title, headerRight }) => (
   <View style={styles.container}>
     <Text style={styles.title}>{title}</Text>
-    {headerRight}
+    <View style={styles.headerRight}>
+      {headerRight}
+    </View>
   </View>
 )
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     backgroundColor: 'white',
     borderBottomWidth: .5,
     borderBottomColor: 'rgb(224,224,224)',
-    shadowColor: 'rgb(224,224,224)',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    shadowColor: 'rgb(224, 224, 224)',
+    paddingLeft: Platform.OS === 'android' ? 16 : undefined,
   },
   title: {
-    fontFamily: 'sans-serif-medium',
-    fontWeight: 'normal',
-    fontSize: 20,
-    paddingLeft: 16,
+    flex: 1,
+    fontFamily: Platform.OS === 'android' ? 'sans-serif-medium' : undefined,
+    fontWeight: Platform.OS === 'android' ? 'normal' : '600',
+    fontSize: Platform.OS === 'android' ? 20 : 17,
+    textAlign: Platform.OS === 'ios' ? 'center' : undefined,
     paddingVertical: 14.5,
     color: 'rgb(28, 28, 30)'
+  },
+  headerRight: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+
   }
 })
 export default CustomHeader
