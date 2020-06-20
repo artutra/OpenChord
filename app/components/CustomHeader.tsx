f1ee9a84707717b382599c9e43a884070dea70fd
Artur Miranda <ldm.artur@gmail.com>
2020-06-20 18:31:16 -0300

    Add CustomHeader component

---
+++ app/components/CustomHeader.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent, ReactNode } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+interface Props {
+  title: string
+  headerRight: ReactNode
+}
+const CustomHeader: FunctionComponent<Props> = ({ title, headerRight }) => (
+  <View style={styles.container}>
+    <Text style={styles.title}>{title}</Text>
+    {headerRight}
+  </View>
+)
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderBottomWidth: .5,
+    borderBottomColor: 'rgb(224,224,224)',
+    shadowColor: 'rgb(224,224,224)',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  title: {
+    fontFamily: 'sans-serif-medium',
+    fontWeight: 'normal',
+    fontSize: 20,
+    paddingLeft: 16,
+    paddingVertical: 14.5,
+    color: 'rgb(28, 28, 30)'
+  }
+})
+export default CustomHeader
